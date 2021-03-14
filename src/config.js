9632e5c23370e1f9ab9d9e3eebdf5fe01144eff6
SaiKyauk <80118792+nicholas-kham@users.noreply.github.com>
2021-03-15 00:06:01 +0630

    Updated cridentials

--- src/config.js
+++ src/config.js
@@ -7,6 +7,7 @@ export const config = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
